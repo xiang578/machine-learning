c086407509643ad616a2e320ae95dd01d94ab1c7
xiang578 <xiang578@foxmail.com>
2020-10-03 23:37:48 +0800

    add index

---
+++ tftest/fm.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import numpy as np
+
+x_data = np.matrix([
+#    Users  |     Movies     |    Movie Ratings   | Time | Last Movies Rated
+#   A  B  C | TI  NH  SW  ST | TI   NH   SW   ST  |      | TI  NH  SW  ST
+    [1, 0, 0,  1,  0,  0,  0,   0.3, 0.3, 0.3, 0,     13,   0,  0,  0,  0 ],
+    [1, 0, 0,  0,  1,  0,  0,   0.3, 0.3, 0.3, 0,     14,   1,  0,  0,  0 ],
+    [1, 0, 0,  0,  0,  1,  0,   0.3, 0.3, 0.3, 0,     16,   0,  1,  0,  0 ],
+    [0, 1, 0,  0,  0,  1,  0,   0,   0,   0.5, 0.5,   5,    0,  0,  0,  0 ],
+    [0, 1, 0,  0,  0,  0,  1,   0,   0,   0.5, 0.5,   8,    0,  0,  1,  0 ],
+    [0, 0, 1,  1,  0,  0,  0,   0.5, 0,   0.5, 0,     9,    0,  0,  0,  0 ],
+    [0, 0, 1,  0,  0,  1,  0,   0.5, 0,   0.5, 0,     12,   1,  0,  0,  0 ]
+])
+# ratings
+y_data = np.array([5, 3, 1, 4, 5, 1, 5])
+# Let's add an axis to make tensoflow happy.
+y_data.shape += (1, )
+
+print(x_data.shape)
+print(y_data.shape)
+
+x = tf.palceholder()
+tf.palceholder()
